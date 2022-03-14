f233f792defe89e6d1428c93999772dfb65aa5ff
Rick Poleshuck <RickPoleshuck@gmail.com>
2022-03-14 07:52:31 -0600

    first

---
+++ bin/flutter_screenshot_writer.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:args/args.dart';
+
+bool verbose = false;
+late Directory outputDir;
+
+void main(List<String> argv) async {
+  late int portNo;
+  final argParser = ArgParser(allowTrailingOptions: false);
+  argParser.addOption('port', abbr: 'p', defaultsTo: '3013', help: 'port to listen on');
+  argParser.addOption('output_dir', abbr: 'o', defaultsTo: Directory.current.path, help: 'Output directory');
+  argParser.addFlag('verbose', abbr: 'v', defaultsTo: true, help: 'verbose');
+  final args = argParser.parse(argv);
+  try {
+    portNo = int.parse(args['port']);
+    outputDir = Directory(args['output_dir']);
+  } on FormatException catch (_) {
+    stderr.write("${argParser.usage}\n");
+    exit(1);
+  }
+  if (args.rest.isNotEmpty) {
+    stderr.write("${argParser.usage}\n");
+    exit(1);
+  }
+  verbose = args['verbose'];
+
+  final server = await ServerSocket.bind(InternetAddress.loopbackIPv4, portNo);
+
+  server.listen((client) {
+    handleConnection(client);
+  });
+}
+
+void saveScreen(dynamic msg) async {
+  String path = msg['path'];
+  if (verbose) {
+    print('Saving screen to: $path');
+  }
+  List<int> image = msg['image'].cast<int>();
+  File f = File(path);
+  f.parent.createSync(recursive: true);
+  f.writeAsBytesSync(image);
+}
+
+void handleConnection(Socket client) {
+  var fullMessage = BytesBuilder();
+  client.listen(
+    (Uint8List data) async {
+      fullMessage.add(data);
+    },
+    onError: (error) {
+      if (verbose) print(error);
+      client.close();
+    },
+    onDone: () {
+      final stringData = String.fromCharCodes(fullMessage.toBytes());
+      final msg = json.decode(stringData);
+      saveScreen(msg);
+      client.close();
+    },
+  );
+}
